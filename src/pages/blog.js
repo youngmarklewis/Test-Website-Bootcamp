ad2d524005234385b3d2fce557a0bdaebc99644a
youngmarklewis <youngmarklewis@gmail.com>
2020-05-26 21:22:05 +0100

    second commit

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -29,7 +29,7 @@ const BlogPage = () => {
       <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(edge => {
           return (
-            <li className={blogStyles.post  }>
+            <li className={blogStyles.post}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
---
+++ src/posts/test.md
@@ -0,0 +1,14 @@
+---
+title: "My second blog post"
+date: "10-05-2020"
+---
+
+I just launched a new website!
+
+![blog](./blog.png)
+
+## Topics Covered
+
+1. Stuff
+2. More Stuff
+3. Stuff Stuff Stuff
