0910bb43bf9ebf2e2814fb8bcf8137bb3c723bd3
youngmarklewis <youngmarklewis@gmail.com>
2020-05-27 13:33:09 +0100

    contenftul post query

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -4,35 +4,64 @@ import { Link, graphql, useStaticQuery } from "gatsby"
 import Layout from "../components/layout"
 import blogStyles from "./blog.module.scss"
 
+// const BlogPage = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             frontmatter {
+//               title
+//               date
+//             }
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `)
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark {
+    query MyFirstQuery {
+      allContentfulBlogPost(sort: { fields: id, order: ASC }) {
         edges {
           node {
-            frontmatter {
-              title
-              date
-            }
-            fields {
-              slug
-            }
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
           }
         }
       }
     }
   `)
-  //console.log(data)
+  // return (
+  // <Layout>
+  //   <h1>Blog</h1>
+  //   <ol className={blogStyles.posts}>
+  //     {data.allMarkdownRemark.edges.map(edge => {
+  //       return (
+  //         <li className={blogStyles.post}>
+  //           <Link to={`/blog/${edge.node.fields.slug}`}>
+  //             <h2>{edge.node.frontmatter.title}</h2>
+  //             <p>{edge.node.frontmatter.date}</p>
+  //           </Link>
+  //         </li>
+  //       )
+  //     })}
+  //   </ol>
+  // </Layout>
   return (
     <Layout>
       <h1>Blog</h1>
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map(edge => {
+        {data.allContentfulBlogPost.edges.map(edge => {
           return (
             <li className={blogStyles.post}>
-              <Link to={`/blog/${edge.node.fields.slug}`}>
-                <h2>{edge.node.frontmatter.title}</h2>
-                <p>{edge.node.frontmatter.date}</p>
+              <Link to={`/blog/${edge.node.slug}`}>
+                <h2>{edge.node.title}</h2>
+                <p>{edge.node.publishedDate}</p>
               </Link>
             </li>
           )
