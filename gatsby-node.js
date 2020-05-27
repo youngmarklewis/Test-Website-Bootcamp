4d6f9ab21ac20d7295dbf095101180d72ddb61a8
youngmarklewis <youngmarklewis@gmail.com>
2020-05-27 15:48:35 +0100

    contentful blog pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,44 +1,76 @@
 const path = require("path")
 
-module.exports.onCreateNode = ({ node, actions }) => {
-  const { createNodeField } = actions
+// onCreateNode was used to generate slugs for the original basic markdown posts
+// This is no longer required as we have created this field in Contentful
 
-  if (node.internal.type === "MarkdownRemark") {
-    const slug = path.basename(node.fileAbsolutePath, ".md")
-    //console.log("@@@@@@@@@@@", slug)
-    //console.log(JSON.stringify(node, undefined, 4))
+// module.exports.onCreateNode = ({ node, actions }) => {
+//   const { createNodeField } = actions
 
-    createNodeField({
-      node,
-      name: "slug",
-      value: slug,
-    })
-  }
-}
+//   if (node.internal.type === "MarkdownRemark") {
+//     const slug = path.basename(node.fileAbsolutePath, ".md")
+
+//     createNodeField({
+//       node,
+//       name: "slug",
+//       value: slug,
+//     })
+//   }
+// }
 
 module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   const blogTemplate = path.resolve("./src/templates/blog.js")
   const res = await graphql(`
     query {
-      allMarkdownRemark {
+      allContentfulBlogPost {
         edges {
           node {
-            fields {
-              slug
-            }
+            slug
           }
         }
       }
     }
   `)
-  res.data.allMarkdownRemark.edges.forEach(edge => {
+
+  // We need to adjust createPages
+  // Instead of fetching the markdown slugs to createPages we want to use
+  // the contentful slugs and use them
+
+  // module.exports.createPages = async ({ graphql, actions }) => {
+  //   const { createPage } = actions
+  //   const blogTemplate = path.resolve("./src/templates/blog.js")
+  //   const res = await graphql(`
+  //     query {
+  //       allMarkdownRemark {
+  //         edges {
+  //           node {
+  //             fields {
+  //               slug
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //   `)
+  res.data.allContentfulBlogPost.edges.forEach(edge => {
     createPage({
       component: blogTemplate,
-      path: `/blog/${edge.node.fields.slug}`,
+      path: `/blog/${edge.node.slug}`,
       context: {
-        slug: edge.node.fields.slug,
+        slug: edge.node.slug,
       },
     })
   })
 }
+// When we loop through the data we can now use the contentful slug above
+
+//   res.data.allMarkdownRemark.edges.forEach(edge => {
+//     createPage({
+//       component: blogTemplate,
+//       path: `/blog/${edge.node.fields.slug}`,
+//       context: {
+//         slug: edge.node.fields.slug,
+//       },
+//     })
+//   })
+// }
--- src/templates/blog.js
+++ src/templates/blog.js
@@ -5,26 +5,50 @@ import Layout from "../components/layout"
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
-      }
-      html
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
     }
   }
 `
 
+// The old query for markdown
+
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
+
+// This code renders our content to the blog slug
+
 const Blog = props => {
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.publishedDate}</p>
     </Layout>
   )
 }
+
+// Old blog component
+
+// const Blog = props => {
+//   return (
+//     <Layout>
+//       <h1>{props.data.markdownRemark.frontmatter.title}</h1>
+//       <p>{props.data.markdownRemark.frontmatter.date}</p>
+//       <div
+//         dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
+//       ></div>
+//     </Layout>
+//   )
+// }
 
 export default Blog
